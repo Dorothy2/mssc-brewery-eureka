4980cf1ea29be1f5ff90a8fb461e421b35c27f15
Dorothy Rifai <drifai28@gmail.com>
2020-08-09 18:23:39 -0400

    Configuration changes to Eureka server

--- src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/MsscBreweryEurekaApplication.java
+++ src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/MsscBreweryEurekaApplication.java
@@ -1,9 +1,11 @@
-package com.example.demo;
+package guru.sfg.brewery.eureka.msscbreweryeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MsscBreweryEurekaApplication {
 
 	public static void main(String[] args) {
